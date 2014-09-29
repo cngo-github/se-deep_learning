0dff7504cadcd05c9669600c0e3f49e7407a4ebc
Chuong Ngo <cngo@localhost.localdomain>
2014-09-28 23:58:37 -0400

    Initial commit.  Added feed-forward neural network based on tutorial to get a basic framework and understanding of THEANO.

---
+++ mlp.py
@@ -0,0 +1,60 @@
+class MLP(object):
+    def __init__(self, W_init, b_init, activations):
+        '''
+        Multi-layer perceptron class, computes the composition of a sequence of Layers
+
+        :parameters:
+            - W_init : list of np.ndarray, len=N
+                Values to initialize the weight matrix in each layer to.
+                The layer sizes will be inferred from the shape of each matrix in W_init
+            - b_init : list of np.ndarray, len=N
+                Values to initialize the bias vector in each layer to
+            - activations : list of theano.tensor.elemwise.Elemwise, len=N
+                Activation function for layer output for each layer
+        '''
+        # Make sure the input lists are all of the same length
+        assert len(W_init) == len(b_init) == len(activations)
+        
+        # Initialize lists of layers
+        self.layers = []
+        # Construct the layers
+        for W, b, activation in zip(W_init, b_init, activations):
+            self.layers.append(Layer(W, b, activation))
+
+        # Combine parameters from all layers
+        self.params = []
+        for layer in self.layers:
+            self.params += layer.params
+        
+    def output(self, x):
+        '''
+        Compute the MLP's output given an input
+        
+        :parameters:
+            - x : theano.tensor.var.TensorVariable
+                Theano symbolic variable for network input
+
+        :returns:
+            - output : theano.tensor.var.TensorVariable
+                x passed through the MLP
+        '''
+        # Recursively compute output
+        for layer in self.layers:
+            x = layer.output(x)
+        return x
+
+    def squared_error(self, x, y):
+        '''
+        Compute the squared euclidean error of the network output against the "true" output y
+        
+        :parameters:
+            - x : theano.tensor.var.TensorVariable
+                Theano symbolic variable for network input
+            - y : theano.tensor.var.TensorVariable
+                Theano symbolic variable for desired network output
+
+        :returns:
+            - error : theano.tensor.var.TensorVariable
+                The squared Euclidian distance between the network output and y
+        '''
+        return T.sum((self.output(x) - y)**2)
