6b6d3814f7c1cb6238542a1ab3409c7e9004127d
Chuong Ngo <cngo.github@gmail.com>
2014-12-04 12:45:49 -0500

    Added file.

--- main.py
+++ main.py
@@ -20,7 +20,7 @@ def main(vocabFile, testFile):
 def run_softmax(seq, targets, n_hidden, n_in, n_steps, n_seq, n_classes, n_out,
 		n_epochs = 250):
 	seq = np.matrix(seq)
-	targets = np.matrix(targets)
+	targets = np.asarray(targets)
 
 	model = Model(logger = logger, n_in=n_in, n_hidden=n_hidden, n_out=n_out,
 			learning_rate=0.001, learning_rate_decay=0.999,
