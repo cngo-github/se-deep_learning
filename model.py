960816d6237f4260e64ff953436538745315317e
Chuong Ngo <cngo@localhost.localdomain>
2014-12-04 09:44:57 -0500

    Initial test commit.

--- model.py
+++ model.py
@@ -1,64 +1,133 @@
-import theano
+from rnn import RNN
+
 import theano.tensor as T
+import numpy as np
 
-import rnn
+import theano
+#import logging
 
-class Model:
-	def __init__(self, params):
-		self.n_input = params['n_input']
-		self.n_hidden = params['n_hidden']
-		self.n_output = params['n_output']
-		self.n_class = params['n_class']
-		self.learning_rate = params['learning_rate'] or 0.01
-		self.n_epochs = params['n_epochs'] or 100
-		self.activation = params['activation'] or 'sigmoid'
+class Model(object):
+	def __init__(self, logger, mode, n_in = 5, n_hidden = 50, n_out = 5,
+			n_cl = 5, learning_rate=0.01, n_epochs=100,
+			learning_rate_decay=1, activation='sigmoid',
+			final_momentum=0.9, initial_momentum=0.5,
+			momentum_switchover=5):
+        	self.n_in = int(n_in)
+		self.n_hidden = int(n_hidden)
+		self.n_out = int(n_out)
+		self.n_cl = int(n_cl)
+		self.learning_rate = float(learning_rate)
+		self.learning_rate_decay = float(learning_rate_decay)
+		self.n_epochs = int(n_epochs)
+		self.activation = activation
+		self.initial_momentum = float(initial_momentum)
+		self.final_momentum = float(final_momentum)
+		self.momentum_switchover = int(momentum_switchover)
+		self.logger = logger
+		self.mode = mode
+
+		self.ready()
 
 	def ready(self):
+		# input (where first dimension is time)
 		self.x = T.matrix()
-		self.y = T.vector(name = 'y')
+
+		# target (where first dimension is time)
+		self.y = T.vector(name='y', dtype='int32')
+
+	        # initial hidden state of the RNN
 		self.h0 = T.vector()
+
+	        # learning rate
 		self.lr = T.scalar()
 
-		if self.activation == 'sigmoid':
+		if self.activation == 'tanh':
+			activation = T.tanh
+		elif self.activation == 'sigmoid':
 			activation = T.nnet.sigmoid
+		elif self.activation == 'relu':
+			activation = lambda x: x * (x > 0)
+		elif self.activation == 'cappedrelu':
+			activation = lambda x: T.minimum(x * (x > 0), 6)
 		else:
 			raise NotImplementedError
 
-		self.rnn = RNN({'input': self.x,
-				'n_input': self.n_input,
-				'n_hidden': self.n_hidden,
-				'n_output': self.n_output,
-				'n_class': self.n_class,
-				'activation': activation})
+		self.rnn = RNN(input=self.x, n_in=self.n_in, n_hidden=self.n_hidden, 
+				n_out=self.n_out, n_cl = self.n_cl,
+				activation=activation)
 
-	def fit(self, x_train, y_train, x_test = None, y_test = None, validation_freq = 100):
-		train_set_x = theano.shared(np.asarray(x_train))
-		train_set_y = theano.shared(np.asarray(y_train))
-		n_train = train_set_x.get_value(borrow = True).shape[0]
+		self.predict_proba = theano.function(inputs=[self.x, ],
+				outputs=self.rnn.y_prob, mode = self.mode)
+		self.predict = theano.function(inputs=[self.x, ],
+				outputs=self.rnn.y_out, mode = self.mode)
 
-		index = T.lscalar('index')
-		l_r = T.scalar('l_r')
+	def shared_dataset(self, data_xy):
+		""" Load the dataset into shared variables """
+
+		data_x, data_y = data_xy
+		shared_x = theano.shared(np.asarray(data_x,
+						dtype=theano.config.floatX))
+
+		shared_y = theano.shared(np.asarray(data_y,
+						dtype=theano.config.floatX))
+
+		return shared_x, T.cast(shared_y, 'int32')
+
+	def fit(self, X_train, Y_train, X_test=None, Y_test=None,
+			validation_frequency=100):
+		""" Fit model
+
+		Pass in X_test, Y_test to compute test error and report during
+		training.
+
+		X_train : ndarray (n_seq x n_steps x n_in)
+		Y_train : ndarray (n_seq x n_steps x n_out)
+
+		validation_frequency : int
+		in terms of number of sequences (or number of weight updates)
+		"""
+		if X_test is not None:
+			assert(Y_test is not None)
+			self.interactive = True
+			test_set_x, test_set_y = self.shared_dataset((X_test, Y_test))
+		else:
+			self.interactive = False
+
+		train_set_x, train_set_y = self.shared_dataset((X_train, Y_train))
+		n_train = train_set_x.get_value(borrow=True).shape[0]
+
+		if self.interactive:
+			n_test = test_set_x.get_value(borrow=True).shape[0]
+
+		######################
+		# BUILD ACTUAL MODEL #
+		######################
+		self.logger.info('... building the model')
+
+		index = T.lscalar('index')    # index to a case
+		# learning rate (may change)
+		l_r = T.scalar('l_r', dtype=theano.config.floatX)
+		mom = T.scalar('mom', dtype=theano.config.floatX)  # momentum
+
 		cost = self.rnn.loss(self.y)
 
-		compute_train_error = theano.function(inputs = [index, ],
-						outputs = self.rnn.lost(self.y),
-						givens = {self.x: train_set_x[index],
-							self.y: train_set_y[index]})
+		compute_train_error = theano.function(inputs=[index, ],
+						outputs=self.rnn.loss(self.y),
+						givens={
+							self.x: train_set_x[index],
+							self.y: train_set_y[index]},
+						mode = self.mode)
 
-		if x_test is not None and y_test is not None:
-			self.test = True
+		if self.interactive:
+			compute_test_error = theano.function(inputs=[index, ],
+					outputs=self.rnn.loss(self.y),
+					givens={
+						self.x: test_set_x[index],
+						self.y: test_set_y[index]},
+					mode = self.mode)
 
-			test_set_x = theano.shared(np.asarray(x_test))
-			test_set_y = theano.shared(np.asarray(y_test))
-			n_test = test_set_x.get_value(borrow = True).shape[0]
-
-			compute_test_error = theano.function(inputs = [index, ],
-							outputs = self.rnn.loss(self.y),
-							givens = {self.x: test_set_x[index],
-								self.y: test_set_y[index]})
-		else:
-			self.teset = False
-
+		# compute the gradient of cost with respect to theta = (W, W_in, W_out)
+		# gradients on the weights using BPTT
 		gparams = []
 		for param in self.rnn.params:
 			gparam = T.grad(cost, param)
@@ -67,28 +136,58 @@ class Model:
 		updates = {}
 		for param, gparam in zip(self.rnn.params, gparams):
 			weight_update = self.rnn.updates[param]
-			updates[weight_update] = weight_update - l_r * gparam
-			updates[param] = param + weight_update - l_r * gparam
+			upd = mom * weight_update - l_r * gparam
+			updates[weight_update] = upd
+			updates[param] = param + upd
 
-		train_model = theano.function(inputs = [index, l_r], outputs = cost,
-						updates = updates,
-						givens = {self.x: train_set_x[index],
-							self.y: train_set_y[index]})
+		# compiling a Theano function `train_model` that returns the
+		# cost, but in the same time updates the parameter of the
+		# model based on the rules defined in `updates`
+		train_model = theano.function(inputs=[index, l_r, mom],
+				outputs=cost,
+				updates=updates,
+				givens={
+					self.x: train_set_x[index],
+					self.y: train_set_y[index]},
+				mode = self.mode)
 
+		###############
+		# TRAIN MODEL #
+		###############
+		self.logger.info('... training')
 		epoch = 0
 
 		while (epoch < self.n_epochs):
-			epoch += 1
-			for i in xrange(x_train):
-				ex_cost = train_model(i, self.learning_rate)
+			epoch = epoch + 1
+			for idx in xrange(n_train):
+				effective_momentum = self.final_momentum \
+						if epoch > self.momentum_switchover \
+						else self.initial_momentum
+			example_cost = train_model(idx, self.learning_rate,
+					effective_momentum)
 
-			j = (epoch - 1) * n_train + i + 1
-			if j % validation_freq == 0:
+			# iteration number (how many weight updates have we made?)
+			# epoch is 1-based, index is 0 based
+			iter = (epoch - 1) * n_train + idx + 1
+
+			if iter % validation_frequency == 0:
+			# compute loss on training set
 				train_losses = [compute_train_error(i)
-						for i in xrange(n_test)]
-				train_loss_mean = np.mean(test_losses)
+							for i in xrange(n_train)]
+				this_train_loss = np.mean(train_losses)
 
-				if self.test:
-					test_losses = [compute_test_error(k)
-							for k in xrange(n_test)]
-					test_loss_mean = np.mean(test_losses)
+				if self.interactive:
+					test_losses = [compute_test_error(i)
+							for i in xrange(n_test)]
+					this_test_loss = np.mean(test_losses)
+
+					self.logger.info('epoch %i, seq %i/%i, tr loss %f '
+						'te loss %f lr: %f' % (epoch, idx + 1, n_train,
+						this_train_loss, this_test_loss,
+						self.learning_rate))
+				else:
+					self.logger.info('epoch %i, seq %i/%i, train loss %f '
+						'lr: %f' % (epoch, idx + 1, n_train,
+						this_train_loss, self.learning_rate))
+
+			self.learning_rate *= self.learning_rate_decay
--- rnn.py
+++ rnn.py
@@ -2,48 +2,89 @@ import numpy as np
 import theano
 import theano.tensor as T
 
-class RNN:
-	def __init__(self, params):
+class RNN(object):
+	"""    Recurrent neural network class
+	Supported output types:
+	real : linear output units, use mean-squared error
+	binary : binary output units, use cross-entropy error
+	softmax : single softmax out, use cross-entropy error
+	"""
+	def __init__(self, input, n_in, n_hidden, n_out, n_cl,
+			activation=T.nnet.sigmoid):	
+		self.input = input
+		self.activation = activation
+
 		# recurrent weights as a shared variable
-		W = np.asarray(np.random.uniform(size=(params['n_hidden'], params['n_hidden']), low=-.01, high=.01))
-		self.W = theano.shared(value = W, name = 'W')
-		
+		W_init = np.asarray(np.random.uniform(size=(n_hidden, n_hidden),
+						low=-.01, high=.01),
+						dtype=theano.config.floatX)
+		self.W = theano.shared(value=W_init, name='W')
+
 		# input to hidden layer weights
-		W_in = np.asarray(np.random.uniform(size=(params['n_input'], params['n_hidden']), low=-.01, high=.01))
-		self.W_in = theano.shared(value = W_in, name = 'W_in')
+		W_in_init = np.asarray(np.random.uniform(size=(n_in, n_hidden),
+						low=-.01, high=.01),
+						dtype=theano.config.floatX)
+		self.W_in = theano.shared(value=W_in_init, name='W_in')
 
-		# hidden to output layer weights
-		W_out = np.asarray(np.random.uniform(size=(params['n_hidden'], params['n_output']), low=-.01, high=.01))
-		self.W_out = theano.shared(value = W_out, name = 'W_out')
+		# hidden to output layer weights (y)
+		W_out_init = np.asarray(np.random.uniform(size=(n_hidden, n_out),
+						low=-.01, high=.01),
+						dtype=theano.config.floatX)
+		self.W_out = theano.shared(value=W_out_init, name='W_out')
 
-		# hidden to class output layer weights
-		W_cl = np.asarray(np.random.uniform(size=(params['n_hidden'], params['n_class']), low=-.01, high=.01))
+		# hidden to output layer weights (class)
+		W_cl = np.asarray(np.random.uniform(size=(n_hidden, n_cl),
+						low=-.01, high=.01),
+						dtype=theano.config.floatX)
 		self.W_cl = theano.shared(value = W_cl, name = 'W_cl')
 
-		# activation function
-		self.activation = params['activation'] or T.nnet.sigmoid
-		self.input = params['input']
+		h0_init = np.zeros((n_hidden,), dtype=theano.config.floatX)
+		self.h0 = theano.shared(value=h0_init, name='h0')
 
-		h0 = np.zeros((n_hidden, ))
-		self.h0 = theano.shared(value = h0, name = 'h0')
+		self.params = [self.W, self.W_in, self.W_out, self.h0]
 
-		self.params = [self.W, self.W_in, self.W_out, self.W_cl, self.h0]
+	        # for every parameter, we maintain it's last update
+        	# the idea here is to use "momentum"
+        	# keep moving mostly in the same direction
+		self.updates = {}
+		for param in self.params:
+			init = np.zeros(param.get_value(borrow=True).shape,
+					dtype=theano.config.floatX)
+			self.updates[param] = theano.shared(init)
 
-		# step function for scan
-		def step(self, x_t, h_tm1):
-			h_t = self.activation(theano.dot(self.W_in, x_t) + theano.dot(self.W, h_tm1))
-			y_t = T.softmax(theano.dot(self.W_out, h_t))
-			cl_t = T.softmax(theano.dot(self.W_cl, h_t))
+		# the hidden state `h` for the entire sequence, and the output for the
+		# entire sequence `y` (first dimension is always time)
+		[self.h, self.y_pred, self.cl_pred], _ = theano.scan(self.step,
+							sequences=self.input,
+							outputs_info=[self.h0, None, None])
 
-		return [h_t, y_t, cl_t]
+		# push through softmax, computing vector of class-membership
+		# probabilities in symbolic form
+		self.y_prob = T.nnet.softmax(self.y_pred)
+		self.cl_prob = T.nnet.softmax(self.cl_pred)
 
-		[self.h, self.y_pred, self.cl_pred], _ = theano.scan(step, \
-						sequences = dict(input = self.input, taps = [-1, 0]), \
-						outputs_info = [dict(self.h0, taps = [-1, 0]), None], \
-						non_sequences = [self.W, self.W_in, self.W_out, self_cl])
+		# compute prediction as class whose probability is maximal
+		self.y_out = T.argmax(self.y_prob, axis=-1)
+		self.cl_prob = T.argmax(self.cl_prob, axis = -1)
+		self.loss = lambda y: self.nll_multiclass(y)
 
-		self.y_out = T.argmax(self.y_pred, self.cl_pred)
-		self.loss = lambda y: self.multiclass(y)
+	def step(self, x_t, h_tm1):
+		h_t = self.activation(T.dot(x_t, self.W_in) + T.dot(h_tm1, self.W))
+		y_t = T.dot(h_t, self.W_out)
+		cl_t = theano.dot(h_t, self.W_cl)
 
-	def multiclass(self, y):
-		return -T.mean(T.log(self.y_pred)[T.arange(y.shape[0]), y])
+		return h_t, y_t, cl_t
+
+	def nll_multiclass(self, y):
+        	# negative log likelihood based on multiclass cross entropy error
+        	# y.shape[0] is (symbolically) the number of rows in y, i.e.,
+        	# number of time steps (call it T) in the sequence
+        	# T.arange(y.shape[0]) is a symbolic vector which will contain
+        	# [0,1,2,... n-1] T.log(self.p_y_given_x) is a matrix of
+        	# Log-Probabilities (call it LP) with one row per example and
+        	# one column per class LP[T.arange(y.shape[0]),y] is a vector
+        	# v containing [LP[0,y[0]], LP[1,y[1]], LP[2,y[2]], ...,
+        	# LP[n-1,y[n-1]]] and T.mean(LP[T.arange(y.shape[0]),y]) is
+        	# the mean (across minibatch examples) of the elements in v,
+        	# i.e., the mean log-likelihood across the minibatch.
+		return -T.mean(T.log(self.y_prob)[T.arange(y.shape[0]), y])
